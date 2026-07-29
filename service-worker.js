85480fe032966eab489b9a996c9de460180a4afa
ruipms <Ruijpms@gmail.com>
2026-07-29 16:47:40 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "lajeosa-cache-v2"; // muda sempre que fizeres deploy
+const CACHE_NAME = "lajeosa-cache-v3"; // muda sempre que fizeres deploy
 
 const FILES_TO_CACHE = [
   "index.html",
