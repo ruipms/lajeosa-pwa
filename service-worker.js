4f97c78a5864539111b8b1973ab97348237fc8df
ruipms <Ruijpms@gmail.com>
2026-08-01 16:00:00 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "lajeosa-cache-alterar pontos e contatos"; // muda sempre que fizeres deploy
+const CACHE_NAME = "lajeosa-cache-alteraração das noticias"; // muda sempre que fizeres deploy
 
 const FILES_TO_CACHE = [
   "index.html",
